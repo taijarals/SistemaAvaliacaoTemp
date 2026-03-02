232aa1b0b830acc261367c0104f3b2bcbb344a8e
taijarals <taijara@outlook.com.br>
2026-03-02 10:01:38 -0300

    secrets

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import streamlit as st
 from supabase import create_client
-import os
-from dotenv import load_dotenv
 import pandas as pd
 
-load_dotenv()
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
